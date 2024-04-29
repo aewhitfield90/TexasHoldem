32970ee1e92a58eca1e1f5b4c1d1bef1e3d48600
Jose <43507345+jfrsm21@users.noreply.github.com>
2024-04-28 22:56:49 -0500

    Added new ShortDeck class which is a subclass of Deck.

--- DeckBuilder.py
+++ DeckBuilder.py
@@ -7,16 +7,27 @@ ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
 class Card:
     """ Represents a single playing card, identified by its suits and rank. """
-    def __init__(self, suit, rank):
-        """ Initialize a new card with given suit and rank, and load its images. """
+    def __init__(self, suit, rank, load_images=True):
+        """ Initialize a new card with given suit and rank, and load its images.
+        Args:
+            suit (str): The suit of the card (H, D, C, S)
+            rank (str): The rank of the card (2-10, J, Q, K, A)
+            load_images (bool): Whether to load the card images or not
+        """ 
         self.suit = suit
         self.rank = rank
+        self.load_images = load_images
         self.show = True   # Determines if the card's face is shown or the back
         self.position = (500, 500)   # Default position on the screen 
         self.id = f"{self.rank}{self.suit}"   # Unique identifier for the card
-        self.img = f"DeckOfCards/{self.id}.png"   # Path to the card's face image
-        self.card_img = pygame.image.load(self.img).convert_alpha()
-        self.back_img = pygame.image.load(f"DeckOfCards/BACK.png").convert_alpha()   # Load the face image
+        
+        if load_images:
+            self.img = f"DeckOfCards/{self.id}.png"   # Path to the card's face image
+            self.card_img = pygame.image.load(self.img).convert_alpha()
+            self.back_img = pygame.image.load(f"DeckOfCards/BACK.png").convert_alpha()   # Load the face image
+        else:
+            self.card_img = None
+            self.back_img = None
 
     # function for printing cards into pygame screen
     def render_card(self, screen):
@@ -35,12 +46,13 @@ class Card:
 
 class Deck:
     """ Represents a deck of playing cards. """
-    def __init__(self): 
+    def __init__(self, load_images=True): 
         """ Initialize the deck by generating a shuffling a full deck of cards. """
-        self.deck = self.generate_deck()
+        self.load_images = load_images
+        self.deck = self.generate_deck(load_images)
         self.shuffle_deck()
 
-    def generate_deck(self):  
+    def generate_deck(self, load_images=True):  
         """ Generate a full deck of 52 cards """
         return [Card(suit, rank) for suit in suits for rank in ranks]
         
@@ -55,4 +67,9 @@ class Deck:
         else:
             raise ValueError("Deck is empty!")
 
-    
+class ShortDeck(Deck):
+    def generate_deck(self, load_images=True):
+        """Generate a deck consisting only of high cards (T, J, Q, K, A)."""
+        suits = ['H', 'D', 'C', 'S']
+        ranks = ["T", "J", "Q", "K", "A"]
+        return [Card(suit, rank, load_images) for suit in suits for rank in ranks]    
